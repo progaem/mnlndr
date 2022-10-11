7a2a04c42f74e356ab7345385cd429f905d6229d
Igor Labutin <igor@labutin.info>
2022-10-11 11:37:20 +0300

    Added Win key for right hand

--- keymap.c
+++ keymap.c
@@ -222,7 +222,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, EN_Y,    EN_U,    EN_I,    EN_O,    EN_P,    EN_BSLS, 
     XXXXXXX, EN_H,    EN_J,    EN_K,    EN_L,    EN_SCLN, EN_QUOT, 
              EN_N,    EN_M,    EN_COMM, EN_DOT,  EN_SLSH, XXXXXXX, 
-                      CMB_LYR, CMB_LYG, XXXXXXX, XXXXXXX, TT_SPCL, 
+                      CMB_LYR, CMB_LYG, WIN_EN,  XXXXXXX, TT_SPCL, 
                       CMB_ALT, /* RIGHT RED THUMB KEY */ 
                       CMB_LAN, CMB_ENT, CMB_SPC /* RIGHT THUMB KEYS */ 
   ),
@@ -243,7 +243,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, EN_S_Y,  EN_S_U,  EN_S_I,  EN_S_O,  EN_S_P,  EN_PIPE, 
     XXXXXXX, EN_S_H,  EN_S_J,  EN_S_K,  EN_S_L,  EN_COLN, EN_DQUO, 
              EN_S_N,  EN_S_M,  EN_LT,   EN_GT,   EN_QUES, XXXXXXX, 
-                      CMB_LYR, CMB_LYG, XXXXXXX, XXXXXXX, TT_SPCL, 
+                      CMB_LYR, CMB_LYG, WIN_EN,  XXXXXXX, TT_SPCL, 
                       CMB_ALT, /* RIGHT RED THUMB KEY */ 
                       CMB_LAN, CMB_ENT, CMB_SPC /* RIGHT THUMB KEYS */ 
   ),
@@ -264,7 +264,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, RU_N,    RU_G,    RU_SH,   RU_SC,   RU_Z,    RU_H, 
     XXXXXXX, RU_R,    CMS_O,   RU_L,    RU_D,    RU_ZH,   RU_E, 
              RU_T,    CMS_SF,  RU_B,    RU_JU,   RU_DOT,  XXXXXXX, 
-                      CMB_LYR, CMB_LYG, XXXXXXX, XXXXXXX, TT_SPCL, 
+                      CMB_LYR, CMB_LYG, WIN_EN,  XXXXXXX, TT_SPCL, 
                       CMB_ALT, /* RIGHT RED THUMB KEY */ 
                       CMB_LAN, CMB_ENT, CMB_SPC /* RIGHT THUMB KEYS */ 
   ),
@@ -284,7 +284,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, RU_S_N,  RU_S_G,  RU_S_SH, RU_S_SC, RU_S_Z,  RU_S_H, 
     XXXXXXX, RU_S_R,  CMS_S_O, RU_S_L,  RU_S_D,  RU_S_ZH, RU_S_E, 
              RU_S_T,  CMS_SSF, RU_S_B,  RU_S_JU, RU_COMM, XXXXXXX, 
-                      CMB_LYR, CMB_LYG, XXXXXXX, XXXXXXX, TT_SPCL, 
+                      CMB_LYR, CMB_LYG, WIN_EN,  XXXXXXX, TT_SPCL, 
                       CMB_ALT, /* RIGHT RED THUMB KEY */ 
                       CMB_LAN, CMB_ENT, CMB_SPC /* RIGHT THUMB KEYS */ 
   ),
