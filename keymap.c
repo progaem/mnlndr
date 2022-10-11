46c80fed21fba8176ae200e03f165e69dafeb680
Igor Labutin <igor@labutin.info>
2022-10-11 11:28:16 +0300

    Moved ` to the right of T, moved Tab back under Esc, added OnceShift to the left of A

--- keymap.c
+++ keymap.c
@@ -209,8 +209,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_EN] = MY_layout( 
     /* LEFT HALF */ 
     KC_ESC,  AG_1,    AG_2,    AG_3,    AG_4,    AG_5,    XXXXXXX, 
-    EN_GRV,  EN_Q,    EN_W,    EN_E,    EN_R,    EN_T,    XXXXXXX, 
-    XXXXXXX, EN_A,    EN_S,    EN_D,    EN_F,    EN_G,    EN_EQL, 
+    KC_TAB,  EN_Q,    EN_W,    EN_E,    EN_R,    EN_T,    EN_GRV, 
+    SFT_N_O, EN_A,    EN_S,    EN_D,    EN_F,    EN_G,    EN_EQL, 
     SFT_N_O, EN_Z,    EN_X,    EN_C,    EN_V,    EN_B, 
     KC_LCTL, WIN_EN,  ALT_EN,  CMB_LYV, CMB_LYG, 
     CMB_DEL, /* LEFT RED THUMB KEY */ 
@@ -230,8 +230,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_EN_S] = MY_layout( 
     /* LEFT HALF */ 
     KC_ESC,  EN_EXCL, EN_AT,   EN_HASH, EN_DLR,  EN_PERC, EN_TILD, 
-    EN_GRV,  EN_S_Q,  EN_S_W,  EN_S_E,  EN_S_R,  EN_S_T,  XXXXXXX, 
-    XXXXXXX, EN_S_A,  EN_S_S,  EN_S_D,  EN_S_F,  EN_S_G,  EN_EQL, 
+    KC_TAB,  EN_S_Q,  EN_S_W,  EN_S_E,  EN_S_R,  EN_S_T,  EN_GRV, 
+    SFT_N_O, EN_S_A,  EN_S_S,  EN_S_D,  EN_S_F,  EN_S_G,  EN_EQL, 
     SFT_N_O, EN_S_Z,  EN_S_X,  EN_S_C,  EN_S_V,  EN_S_B, 
     KC_LCTL, WIN_EN,  ALT_EN,  CMB_LYV, CMB_LYG, 
     CMB_DEL, /* LEFT RED THUMB KEY */ 
@@ -251,8 +251,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_RU] = MY_layout( 
     /* LEFT HALF */ 
     KC_ESC,  AG_1,    AG_2,    AG_3,    AG_4,    AG_5,    RU_JO, 
-    EN_GRV,  RU_J,    RU_TS,   RU_U,    RU_K,    RU_JE,   XXXXXXX, 
-    XXXXXXX, RU_F,    RU_Y,    RU_V,    RU_A,    RU_P,    RU_EQL, 
+    KC_TAB,  RU_J,    RU_TS,   RU_U,    RU_K,    RU_JE,   EN_GRV, 
+    SFT_N_O, RU_F,    RU_Y,    RU_V,    RU_A,    RU_P,    RU_EQL, 
     SFT_N_O, RU_JA,   RU_CH,   RU_S,    RU_M,    RU_I, 
     KC_LCTL, WIN_EN,  ALT_EN,  CMB_LYV, CMB_LYG, 
     CMB_DEL, /* LEFT RED THUMB KEY */ 
@@ -271,8 +271,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_RU_S] = MY_layout( 
     /* LEFT HALF */ 
     KC_ESC,  RU_EXCL, RU_DQUO, RU_NUME, RU_SCLN, RU_PERC, RU_S_JO, 
-    EN_GRV,  RU_S_J,  RU_S_TS, RU_S_U,  RU_S_K,  RU_S_JE, XXXXXXX, 
-    XXXXXXX, RU_S_F,  RU_S_Y,  RU_S_V,  RU_S_A,  RU_S_P,  RU_EQL, 
+    KC_TAB,  RU_S_J,  RU_S_TS, RU_S_U,  RU_S_K,  RU_S_JE, EN_GRV, 
+    SFT_N_O, RU_S_F,  RU_S_Y,  RU_S_V,  RU_S_A,  RU_S_P,  RU_EQL, 
     SFT_N_O, RU_S_JA, RU_S_CH, RU_S_S,  RU_S_M,  RU_S_I, 
     KC_LCTL, WIN_EN,  ALT_EN,  CMB_LYV, CMB_LYG, 
     CMB_DEL, /* LEFT RED THUMB KEY */ 
