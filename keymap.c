d21800eb2d586ae74bb0a50ce11871645ed33575
Igor Labutin <igor@labutin.info>
2022-10-15 23:48:37 +0300

    swap ^ and ;

--- keymap.c
+++ keymap.c
@@ -171,7 +171,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     CMB_SFT, CMB_BSP, CMB_CTL, /* LEFT THUMB KEYS */ 
     
     /* RIGHT HALF */ 
-    AG_SCLN, EN_CIRC, EN_AMPR, AG_ASTR, AG_COLN, CMS_QUE, CMB_SLS, 
+    EN_CIRC, AG_SCLN, EN_AMPR, AG_ASTR, AG_COLN, CMS_QUE, CMB_SLS, 
     CMB_PLU, EN_Y,    EN_U,    EN_I,    EN_O,    EN_P,    EN_BSLS, 
     CMB_MIN, EN_H,    EN_J,    EN_K,    EN_L,    EN_SCLN, EN_QUOT, 
              EN_N,    EN_M,    EN_COMM, EN_DOT,  EN_SLSH, XXXXXXX, 
@@ -192,7 +192,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     CMB_SFT, CMB_BSP, CMB_CTL, /* LEFT THUMB KEYS */ 
     
     /* RIGHT HALF */ 
-    EN_TILD, EN_CIRC, EN_AMPR, AG_ASTR, AG_COLN, CMS_QUE, EN_BSLS, 
+    EN_TILD, AG_SCLN, EN_AMPR, AG_ASTR, AG_COLN, CMS_QUE, EN_BSLS, 
     CMB_PLU, EN_S_Y,  EN_S_U,  EN_S_I,  EN_S_O,  EN_S_P,  EN_PIPE, 
     CMB_MIN, EN_S_H,  EN_S_J,  EN_S_K,  EN_S_L,  EN_COLN, EN_DQUO, 
              EN_S_N,  EN_S_M,  EN_LT,   EN_GT,   EN_QUES, XXXXXXX, 
@@ -213,7 +213,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     CMB_SFT, CMB_BSP, CMB_CTL, /* LEFT THUMB KEYS */ 
     
     /* RIGHT HALF */ 
-    AG_SCLN, EN_CIRC, EN_AMPR, AG_ASTR, AG_COLN, CMS_QUE, CMB_SLS, 
+    EN_CIRC, AG_SCLN, EN_AMPR, AG_ASTR, AG_COLN, CMS_QUE, CMB_SLS, 
     CMB_PLU, RU_N,    RU_G,    RU_SH,   RU_SC,   RU_Z,    RU_H, 
     CMB_MIN, RU_R,    CMS_O,   RU_L,    RU_D,    RU_ZH,   RU_E, 
              RU_T,    CMS_SF,  RU_B,    RU_JU,   RU_DOT,  XXXXXXX, 
@@ -233,7 +233,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     CMB_SFT, CMB_BSP, CMB_CTL, /* LEFT THUMB KEYS */ 
     
     /* RIGHT HALF */ 
-    EN_TILD, EN_CIRC, EN_AMPR, AG_ASTR, AG_COLN, CMS_QUE, EN_BSLS, 
+    EN_TILD, AG_SCLN, EN_AMPR, AG_ASTR, AG_COLN, CMS_QUE, EN_BSLS, 
     CMB_PLU, RU_S_N,  RU_S_G,  RU_S_SH, RU_S_SC, RU_S_Z,  RU_S_H, 
     CMB_MIN, RU_S_R,  CMS_S_O, RU_S_L,  RU_S_D,  RU_S_ZH, RU_S_E, 
              RU_S_T,  CMS_SSF, RU_S_B,  RU_S_JU, RU_COMM, XXXXXXX, 
